40a230c53d639404beff7af3b9d482e2f4dfe0cc
chacal1231 <jedmacmahonve@unal.edu.co>
2018-01-24 21:57:51 +0000

    Added variable to keep alive socket connection

--- Python/isocket.py
+++ Python/isocket.py
@@ -14,15 +14,19 @@ client= ModbusClient(method = "rtu", port="/dev/ttyS0",stopbits = 1, bytesize = 
 #Connect to the serial modbus server
 connection = client.connect()
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.settimeout(None)
+client_socket.setsockopt( socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 client_socket.connect(('181.129.168.242', 7774))
+
 while 1:
     data = client_socket.recv(512)
     if ( data == 'Command1\r\n'):
 	#Start DAU Testing
 	DAU_Start  = client.write_register(40002, 0, unit=0x0a)
 	print "DAU testing start"
-	os.system('python /home/pi/OSS/Python/SendData.py &')
+	#os.system('nohup python /home/pi/OSS/Python/SendData.py &')
     elif ( data == 'Command2\r\n'):
 	print "DAU testing stop"
 	DAU_Stop  = client.write_register(40002, 1, unit=0x0a)
 	os.system('pkill -9 -f /home/pi/OSS/Python/SendData.py')
+
