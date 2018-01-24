bd38d885b6f4c26f9d67b4ada5fbcea00a2dbc82
chacal1231 <jedmacmahonve@unal.edu.co>
2018-01-24 15:07:28 -0500

    Fix format in date and hour variables.

--- Web/auto.php
+++ Web/auto.php
@@ -197,8 +197,8 @@ while (true) {
 				}
 				else if(strpos($data,"WCUT=")!==false){
 					$WCUT=str_replace("WCUT=","",$data);
-					$Date = date ('Y-m-d H:i:s');
-					$Hour = date ('h:i:s A');
+					$Date = date ('Y-m-d');
+					$Hour = date ('h:i:s');
 					mysqli_query($link,"INSERT INTO minutedata(Datex,hour,LFR,WFR,OFR,GFR,TMP,PRE,WCUT) VALUES('$Date','$Hour','$LFR', '$WFR', '$OFR', '$GFR', '$TMP', '$PRE', '$WCUT')");
 				}
 				#Accumulate RX Data
@@ -213,8 +213,8 @@ while (true) {
 				}
 				else if(strpos($data,"AGxFR=")!==false){
 					$AGFR=str_replace("AGxFR=","",$data);
-					$Date = date ('Y-m-d H:i:s');
-					$Hour = date ('h:i:s A');
+					$Date = date ('Y-m-d');
+					$Hour = date ('h:i:s');
 					mysqli_query($link,"INSERT INTO accdata(date,hour,ALFR,AWFR,AOFR,AGFR) VALUES('$Date','$Hour','$ALFR', '$AWFR', '$AOFR', '$AGFR')");
 				}
 
@@ -251,8 +251,8 @@ while (true) {
 				}
 				else if(strpos($data,"AAvGOR=")!==false){
 					$AAGOR=str_replace("AAvGOR=","",$data);
-					$Date = date ('Y-m-d H:i:s');
-					$Hour = date ('h:i:s A');
+					$Date = date ('Y-m-d');
+					$Hour = date ('h:i:s');
 					mysqli_query($link,"INSERT INTO avedata(date,hour,AALFR,AAWFR,AAOFR,AAGFR,AAWCT,AAGVF,AATMP,AAPRES,AAIPRES,AAOPRES,AAGOR) VALUES('$Date','$Hour','$AALFR', '$AAWFR', '$AAOFR', '$AAGFR', '$AAWCT', '$AAGVF', '$AATMP', '$AAPRES', '$AAIPRES', '$AAOPRES', '$AAGOR')");
 				}
 				else if(strpos($data,"C=1")!==false){
